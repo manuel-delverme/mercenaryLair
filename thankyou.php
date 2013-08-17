323e20646d822b22d695a76b0a68ca4af99c339a
manuel.delverme@gmail.com <Manuel.DelVerme@gmail.com>
2013-08-17 02:09:38 +0200

    added launch page and thankyou.php, not finished

---
+++ thankyou.php
@@ -0,0 +1,17 @@
+<?php echo '<pre>'; print_r(get_defined_vars()); echo '</pre>'; ?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Coming Soon Splash Page CSS3</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="coming-soon">
+      <h1>Thank you</h1>
+      <h2> you'll get a mail as soon as we start the beta phase</h2>
+      <br/>
+      <a href="http://facebook.com/offended" title="Visit our Facebook Page" class="facebook center">Pros 4 hire</a>
+    </div>
+  </body>
+</html>
